09474a01c14708f065b7505386655e436f273d57
Fuexfollets <fuex9follets@gmail.com>
2023-06-05 23:57:59 +0000

    Operated GameLoop.greet() for main game loop

--- Main.java
+++ Main.java
@@ -3,10 +3,6 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		GameLoop gameLoop = new GameLoop();
-		gameLoop.startGame();
-
-		
-		
-		// gameLoop.startGame();
+		gameLoop.greet();
 	}
 }
